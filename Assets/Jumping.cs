d87896e5acb243b9a3bb3480f01692997cf78bcf
MonikaB <monika2.bukovics@live.uwe.ac.uk>
2025-02-09 14:46:32 +0000

    added assets for background

---
+++ Assets/Jumping.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Jumping : MonoBehaviour
+{
+    public float jumpForce = 5f;        
+    public float gravity = -9.8f;       
+    private bool isGrounded = true;     
+    private float verticalVelocity = 0;
+
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
+        {
+            verticalVelocity = jumpForce;
+            isGrounded = false;
+        }
+
+        if (!isGrounded)
+        {
+            verticalVelocity += gravity * Time.deltaTime;
+            transform.position += new Vector3(0, verticalVelocity * Time.deltaTime, 0);
+
+            if (transform.position.y <= 0)
+            {
+                transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+                verticalVelocity = 0;
+                isGrounded = true;
+            }
+        }
+    }
+}
